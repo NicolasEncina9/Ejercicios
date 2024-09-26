1cef6561b348659bedd7fc70b68f9e62552db8fa
Nicolas.Encina <encina95@hotmail.com>
2024-09-26 15:37:57 -0300

    Descripción: Dado el número a = 15, determina si es par o impar. Un número es par si es divisible por 2, y es impar si no lo es.
    Variables: a = 15.

--- src/ejercicios/CalcularProducto.java
+++ src/ejercicios/CalcularProducto.java
@@ -1,4 +1,13 @@
 package ejercicios;
 
 public class CalcularProducto {
+    public static void main(String [] args) {
+
+
+        int A = 7;
+        int B = 8;
+        int producto = A * B;
+
+        System.out.println("El producto de 7x8 es " + producto);
+    }
 }
---
+++ src/ejercicios/NumeroParOimpar.java
@@ -0,0 +1,10 @@
+package ejercicios;
+
+public class NumeroParOimpar {
+    public static void main(String[] args){
+
+
+
+
+    }
+}
