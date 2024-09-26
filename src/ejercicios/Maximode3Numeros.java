4c324fc00697f69e6f34a92bf060ce9179057aca
Nicolas.Encina <encina95@hotmail.com>
2024-09-26 20:38:40 -0300

    Descripción: Dados los números a = 5, b = 12 y c = 9, encuentra cuál de ellos es el mayor.

---
+++ src/ejercicios/Maximode3Numeros.java
@@ -0,0 +1,21 @@
+package ejercicios;
+
+public class Maximode3Numeros {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 12;
+        int c = 9;
+
+        if (b == b) {
+            System.out.println("El mayor es " + b);
+        if (a < b);
+            System.out.println("No es mayor.");
+            if (c < b);
+            System.out.println("No es mayor.");
+        }
+
+
+
+    }
+}
