ea201cbff21d9ac233c9f2e610484a92e87826e1
Nicolas.Encina <encina95@hotmail.com>
2024-09-26 15:52:36 -0300

    Descripción: Dado el número a = 29, determina si es primo. Un número primo solo tiene dos divisores: 1 y él mismo.

---
+++ src/ejercicios/NumerosPrimos.java
@@ -0,0 +1,16 @@
+package ejercicios;
+
+public class NumerosPrimos {
+    public static void main(String[] args){
+
+        int a = 29;
+
+        if (a % 2 == 0){
+            System.out.println(a + " es numero par." );
+        }else {
+            System.out.println(a + " es numero impar. ");
+        }
+    }
+}
+
+}
