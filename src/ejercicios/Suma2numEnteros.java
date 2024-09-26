2211ca56e8b1db76f0ffbe9e5cb6cf03ca2fbdfe
Nicolas.Encina <encina95@hotmail.com>
2024-09-25 21:29:37 -0300

    Se crea estructura de projecto y el primer ejercicio a modod de prueba para el uso de git con el IDE

--- src/ejercicios/Suma2numEnteros.java
+++ src/ejercicios/Suma2numEnteros.java
@@ -1,5 +1,17 @@
 package ejercicios;
 
 public class Suma2numEnteros {
+    public static void main(String[] args){
+
+        int numero1 = 10;
+        int numero2 = 20;
+        int resultado = numero1 + numero2;
+
+        System.out.println("La suma de los sumeros da " + resultado);
+
+
+
+
+    }
 
 }
