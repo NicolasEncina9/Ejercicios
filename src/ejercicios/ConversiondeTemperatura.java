1c721635608806f04d4752ff9d5fe4adb5c08d9c
Nicolas.Encina <encina95@hotmail.com>
2024-09-26 22:00:00 -0300

    escripción: Dado que la temperatura es c = 25 grados Celsius, conviértela a Fahrenheit usando la fórmula:

---
+++ src/ejercicios/ConversiondeTemperatura.java
@@ -0,0 +1,7 @@
+package ejercicios;
+//escripción: Dado que la temperatura es c = 25 grados Celsius, conviértela a Fahrenheit usando la fórmula:
+public class ConversiondeTemperatura {
+    public static void main(String[] args) {
+
+    }
+}
