30322bc814a075d849475ad3e51f35efcd0090f4
Nicolas.Encina <encina95@hotmail.com>
2024-09-26 21:20:34 -0300

    Descripción: Dados los números a = 4, b = 9 y c = 13, calcula el promedio de los tres.

---
+++ src/ejercicios/PromedioDe3Numeros.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class PromedioDe3Numeros {
+    public static void main(String[] args) {
+
+        int Nota1 =7;
+        int Nota2 =8;
+        int Nota3 =4;
+
+
+    }
+}
