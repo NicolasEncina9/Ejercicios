5d41272e917eaebf96555efd36556584d7653e9f
Nicolas.Encina <encina95@hotmail.com>
2024-09-27 09:01:54 -0300

    Descripción: Suma todos los números pares entre a = 1 y b = 100. Un número es par si es divisible por 2.
    Variables: a = 1, b = 100.

---
+++ src/ejercicios/SumadeNumerosPares.java
@@ -0,0 +1,16 @@
+package ejercicios;
+//Descripción: Suma todos los números pares entre a = 1 y b = 100. Un número es par si es divisible por 2.
+//
+//Variables: a = 1, b = 100.
+public class SumadeNumerosPares {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 100;
+
+        for (int i = 2; i<2; i++);{
+            a += i;
+        }
+
+    }
+}
