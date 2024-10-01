28db2599b9abbb5dee21645cecedcddada809f33
Nicolas.Encina <encina95@hotmail.com>
2024-10-01 20:57:13 -0300

    Descripción: Dado el número N = 50, calcula la suma de los primeros N números naturales. Los números naturales son los enteros positivos comenzando desde 1.
    Variables: N = 50.

---
+++ src/ejercicios/SumadeNumerosNaturales.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+public class SumadeNumerosNaturales {
+    public static void main(String[] args) {
+
+        int N = 50;
+
+        while (N <= 50) {
+            if (N == 1) {
+                N++;
+                System.out.println(N);
+                N++;
+            }
+        }
+    }
+}
+
