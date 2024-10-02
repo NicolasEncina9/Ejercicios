a58cace79d0282f4c75b8258e75dd58ca7a89922
Nicolas.Encina <encina95@hotmail.com>
2024-10-02 09:33:31 -0300

    Descripción: Dado el número a = 6, calcula su factorial. El factorial de un número se obtiene multiplicando todos los números desde 1 hasta el número dado.
    Variables: a = 6.

---
+++ src/ejercicios/FactorialdeUnNumero.java
@@ -0,0 +1,18 @@
+package ejercicios;
+//Descripción: Dado el número a = 6, calcula su factorial. El factorial de un número se obtiene multiplicando
+// todos los números desde 1 hasta el número dado.
+//
+//Variables: a = 6.
+
+public class FactorialdeUnNumero {
+    public static void main(String[] args) {
+
+        int a = 6;
+        long factorial = calcularFactorial(int numero) {
+            long resultado = 1;
+            for (int i = 1; i<= numero; ++){
+                resultado += i;
+            }
+        }
+    }
+}
